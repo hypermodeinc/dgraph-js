02b407862aba0e976db1c85762bf878da3796e29
Ryan Fox-Tyler <60440289+ryanfoxtyler@users.noreply.github.com>
2025-04-06 09:46:56 -0400

    move to Biome for linting

--- lib/dgraph.d.ts
+++ lib/dgraph.d.ts
@@ -3,21 +3,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from "./types"
+export * from "./types";
 export {
-  Operation,
-  Request,
-  TxnContext,
-  Check,
-  Version,
-  NQuad,
-  Value,
-  Facet,
-  Latency,
-} from "../generated/api_pb"
-export * from "./clientStub"
-export * from "./client"
-export * from "./clientStubFromSlash"
-export * from "./txn"
-export * from "./errors"
-export * as grpc from "@grpc/grpc-js"
+	Operation,
+	Request,
+	TxnContext,
+	Check,
+	Version,
+	NQuad,
+	Value,
+	Facet,
+	Latency,
+} from "../generated/api_pb";
+export * from "./clientStub";
+export * from "./client";
+export * from "./clientStubFromSlash";
+export * from "./txn";
+export * from "./errors";
+export * as grpc from "@grpc/grpc-js";
